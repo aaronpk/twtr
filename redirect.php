88ea1c7403fa3cbdf383ca1fe748c3a11f97a90f
Aaron Parecki <aaron@parecki.com>
2014-06-22 15:14:19 -0700

    trim non-base60 characters
    trim some non-base60 characters from the query string before decoding it, in case clients accidentally auto-link a trailing close paren

--- redirect.php
+++ redirect.php
@@ -2,4 +2,4 @@
 require_once('lib.php');
 
 header('HTTP/1.1 301 Moved Permanently');
-header('Location: https://twitter.com/_/status/' . b60to10($_GET['short']));
+header('Location: https://twitter.com/_/status/' . b60to10(trim($_GET['short'],')]"\'., ')));
