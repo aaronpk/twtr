d4680a759830c55aae51ee36c6b224bcf6259dc3
Aaron Parecki <aaron@parecki.com>
2016-12-19 16:15:07 +0000

    copy updates for the home page

--- index.php
+++ index.php
@@ -14,26 +14,24 @@
   </form>
 
 
-<h2>
-<a name="shortening" class="anchor" href="#shortening"><span class="octicon octicon-link"></span></a>Shortening</h2>
+<h2>How it Works</h2>
+<h3><a name="shortening" class="anchor" href="#shortening"><span class="octicon octicon-link"></span></a>Shortening</h3>
 
 <p>The base 10 tweet ID is converted to a <a href="http://ttk.me/w/NewBase60">NewBase60</a> number to compress it. The ID is appended to <code>http://twtr.io/</code> forming the shortened URL for the tweet.</p>
 
 
-<h2>
-<a name="un-shortening" class="anchor" href="#un-shortening"><span class="octicon octicon-link"></span></a>Un-Shortening</h2>
+<h3><a name="un-shortening" class="anchor" href="#un-shortening"><span class="octicon octicon-link"></span></a>Un-Shortening</h3>
 
 <p>To un-shorten a link, the NewBase60 number is converted back to its base 10 equivalent. The base 10 ID is appended to <code>https://twitter.com/_/status/</code>. This takes advantage of a feature of twitter.com where they will redirect a tweet URL to the correct username.</p>
 
 
 
-<h2>
-<a name="faq" class="anchor" href="#un-shortening"><span class="octicon octicon-link"></span></a>FAQ</h2>
+<h2><a name="faq" class="anchor" href="#un-shortening"><span class="octicon octicon-link"></span></a>FAQ</h2>
 
 <h3>Q: Why do I need this?</h3>
 <p>A: You probably don't</p>
 
 <h3>Q: Where is this used?</h3>
-<p>A: You can see an example of where this is useful on the IRC logs for the <a href="http://indiewebcamp.com/irc/2013-08-07#t1375897213">#indiewebcamp IRC channel</a>.</p>
+<p>A: You can see an example of where this is useful on the IRC logs for the <a href="https://indieweb.org/irc/2013-08-07#t1375897213">#indieweb IRC channel</a>.</p>
 
 <?php include('footer.php'); ?>
