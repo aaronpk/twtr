215dcfe6442b1de5d6ff9d4c8deaae5ce1b6ea0b
Aaron Parecki <aaron@parecki.com>
2016-12-19 16:14:59 +0000

    also expand twtr.io URLs

--- shorten.php
+++ shorten.php
@@ -4,6 +4,14 @@ require_once('lib.php');
 if(get('url')) {
   if(preg_match('/statuse?s?\/([0-9]+)/', get('url'), $match)) {
     $id = $match[1];
+    $shortURL = 'http://twtr.io/' . b10to60($id);
+  } elseif(preg_match('/twtr\.io\/([0-9a-z]+)/i', get('url'), $match)) {
+    $twtr = 'https://twitter.com/_/status/' . b60to10($match[1]);
+    $ch = curl_init($twtr);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_exec($ch);
+    $shortURL = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
   } else {
     header('HTTP/1.1 400 Bad Request');
     if(get('format') == 'html') {
@@ -22,8 +30,6 @@ if(get('url')) {
   echo json_encode(array('error'=>'missing_input','error_description'=>'Pass in a full tweet URL into the "url" parameter'));
   die();
 }
-
-$shortURL = 'http://twtr.io/' . b10to60($id);
 
 if(get('format') == 'html') {
 
